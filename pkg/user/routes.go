88454502c3393d661abd3393e90532b011d3fc13
daurelie <aurelien.dugast@ynov.com>
2026-01-03 18:21:27 +0100

    remove users routes protection and add gitignore

--- pkg/user/routes.go
+++ pkg/user/routes.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"vet-clinic-api/config"
-	"vet-clinic-api/pkg/authentication"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -15,17 +14,11 @@ func Routes(configuration *config.Config) chi.Router {
 
 	// Routes definition
 	router.Post("/login", userConfig.LoginHandler)
-
-	// Routes protected by authentication
-	router.Group(func(router chi.Router) {
-		router.Use(authentication.AuthMiddleware(userConfig.JWTSecret))
-
-		router.Get("/{id}", userConfig.GetByIdHandler)
-		router.Get("/", userConfig.GetAllHandler)
-		router.Post("/", userConfig.PostHandler)
-		router.Put("/{id}", userConfig.UpdateHandler)
-		router.Delete("/{id}", userConfig.DeleteHandler)
-	})
+	router.Get("/{id}", userConfig.GetByIdHandler)
+	router.Get("/", userConfig.GetAllHandler)
+	router.Post("/", userConfig.PostHandler)
+	router.Put("/{id}", userConfig.UpdateHandler)
+	router.Delete("/{id}", userConfig.DeleteHandler)
 
 	return router
 }
